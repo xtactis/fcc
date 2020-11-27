53ea3cfbb05fc96823dc52037c64505e15389943
xtactis <xtactis@gmail.com>
2020-11-28 00:40:42 +0100

    added a types header that are easier to type than uint32_t and more explicit than int

---
+++ utils/types.h
@@ -0,0 +1,18 @@
+/* date = November 27th 2020 10:29 pm */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+
+#define s8  int8_t
+#define s16 int16_t
+#define s32 int32_t
+#define s64 int64_t
+
+#define u8  uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+#define u64 uint64_t
+
+#endif //TYPES_H
