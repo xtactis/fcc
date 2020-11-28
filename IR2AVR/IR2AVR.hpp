b63c7926d362f12dd44e6a43d19ecac3e9156a4a
xtactis <xtactis@gmail.com>
2020-11-28 23:20:23 +0100

    finished the printAVR function.. not sure if it'll be useful - also not thoroughly tested, but seems to work

--- IR2AVR/IR2AVR.hpp
+++ IR2AVR/IR2AVR.hpp
@@ -12,104 +12,252 @@ currently the IR registers are 1 to 1 with AVR registers, meaning there's only 3
 */
 
 // NOTE(mdizdar): at some point we might want/need AVR instructions to be >32 bit, I am unaware of the existence of such instructions though
-void printAVR(u32 instruction) {
+void printAVR(const std::vector<u32> &instructions) {
+    // https://en.wikipedia.org/wiki/Atmel_AVR_instruction_set#Instruction_encoding
+    // http://ww1.microchip.com/downloads/cn/DeviceDoc/AVR-Instruction-Set-Manual-DS40002198A.pdf
+    
     // NOTE(mdizdar): I wonder if there's a nicer way of going about this... (there is)
-    if (instruction == 0) {
-        // NOP
-        printf("[0x0000]\tnop\n");
-    } else if ((instruction >> 24) == 1) {
-        // MOVW
+    for (u32 instruction: instructions) {
+        if ((instruction & 0xFC0F0000) == 0x90000000) {
+            // LDS/STS - 32-bit instruction
+            const u16 SRAMaddress = instruction & 0xFFFF;
+            const u16 reg = (instruction & 0x003F00000) >> 16;
+            if (instruction & 0x02000000) {
+                // STS
+                printf("[0x%08x]\tsts r%d, 0x%x\n", instruction, reg, SRAMaddress << 1);
+            } else {
+                // LDS
+                printf("[0x%08x]\tlds 0x%x, r%d\n", instruction, SRAMaddress << 1, reg);
+            }
+            continue;
+        }
+        if ((instruction & 0xFE0C0000) == 0x940C0000) {
+            // JMP/CALL - 32-bit instruction
+            const u32 SRAMaddress = (instruction & 0x1FFFF) | ((instruction & 0x1F00000) >> 3);
+            if (instruction & 0x00020000) {
+                // CALL
+                printf("[0x%08x]\tcall 0x%x\n", instruction, SRAMaddress << 1);
+            } else {
+                // JMP
+                printf("[0x%08x]\tjmp 0x%x\n", instruction, SRAMaddress << 1);
+            }
+            continue;
+        }
+        
+        // from here on it's only 16 bit instructions
         instruction >>= 16;
-        const u16 r1 = instruction & 0xF;
-        const u16 r2 = (instruction & 0xF0) >> 4;
-        printf("[0x%x]\tmovw r%d, r%d\n", instruction, r2+16, r1+16);
-    } else if ((instruction >> 24) == 2) {
-        // MULS
-        instruction >>= 16;
-        const u16 r1 = instruction & 0xF;
-        const u16 r2 = (instruction & 0xF0) >> 4;
-        printf("[0x%x]\tmuls r%d, r%d\n", instruction, r2+16, r1+16);
-    } else if ((instruction & 0xFF880000) == 0x0300000) {
-        // MULSU
-        instruction >>= 16;
-        const u16 r1 = instruction & 0x7;
-        const u16 r2 = (instruction & 0x70) >> 4;
-        printf("[0x%x]\tmulsu r%d, r%d\n", instruction, r2+16, r1+16);
-    } else if ((instruction & 0xFF880000) == 0x0308000) {
-        // FMUL
-        instruction >>= 16;
-        const u16 r1 = instruction & 0x7;
-        const u16 r2 = (instruction & 0x70) >> 4;
-        printf("[0x%x]\tfmul r%d, r%d\n", instruction, r2+16, r1+16);
-    } else if ((instruction & 0xFF880000) == 0x0380000) {
-        // FMULS
-        instruction >>= 16;
-        const u16 r1 = instruction & 0x7;
-        const u16 r2 = (instruction & 0x70) >> 4;
-        printf("[0x%x]\tfmuls r%d, r%d\n", instruction, r2+16, r1+16);
-    } else if ((instruction & 0xFF880000) == 0x0388000) {
-        // FMULSU
-        instruction >>= 16;
-        const u16 r1 = instruction & 0x7;
-        const u16 r2 = (instruction & 0x70) >> 4;
-        printf("[0x%x]\tfmulsu r%d, r%d\n", instruction, r2+16, r1+16);
-    } else if ((instruction >> 30) == 0) {
-        // 2-op instruction
-        instruction >>= 16;
-        const u16 r1 = (instruction & 0xF) | ((instruction & 0x200) >> 5);
-        const u16 r2 = (instruction & 0x1F0) >> 4;
-        const u8 op = (instruction & 0x3C00) >> 10;
-        const char * const opnames[] = {"", "cpc", "sbc", "add", "cpse", "cp", "sub", "adc", "and", "eor", "or", "mov"};
-        printf("[0x%x]\t%s r%d, r%d\n", instruction, opnames[op], r1, r2);
-    } else if ((instruction >> 28) == 0xE) {
-        // LDI
-        instruction >>= 16;
-        const u16 value = ((instruction >> 4) & 0xF0) + (instruction & 0xF);
-        const u16 reg = (instruction >> 4) & 0xF;
-        printf("[0x%x]\tldi r%d, 0x%x\n", instruction, reg+16, value);
-    } else if ((instruction & 0xFE0F0000) == 0x900F0000) {
-        // POP
-        instruction >>= 16;
-        const u16 reg = (instruction & 0x01F0) >> 4;
-        printf("[0x%x]\tpop r%d\n", instruction, reg);
-    } else if ((instruction & 0xFE0F0000) == 0x920F0000) {
-        // PUSH
-        instruction >>= 16;
-        const u16 reg = (instruction & 0x01F0) >> 4;
-        printf("[0x%x]\tpush r%d\n", instruction, reg);
-    } else if ((instruction >> 28) == 0x4) {
-        // SBCI
-        instruction >>= 16;
-        const u16 value = ((instruction >> 4) & 0xF0) + (instruction & 0xF);
-        const u16 reg = (instruction >> 4) & 0xF;
-        printf("[0x%x]\tsbci r%d, 0x%x\n", instruction, reg+16, value);
-    } else if (instruction == 0x95080000) {
-        // RET
-        printf("[0x9508]\tret\n");
-    } else {
-        error("You have invented a new AVR instruction, gz");
+        
+        if (instruction == 0) {
+            // NOP
+            printf("[0x0000]\tnop\n");
+        } else if ((instruction >> 8) == 1) {
+            // MOVW
+            const u16 r1 = instruction & 0xF;
+            const u16 r2 = (instruction & 0xF0) >> 4;
+            printf("[0x%04x]\tmovw r%d, r%d\n", instruction, r2+16, r1+16);
+        } else if ((instruction >> 8) == 2) {
+            // MULS
+            const u16 r1 = instruction & 0xF;
+            const u16 r2 = (instruction & 0xF0) >> 4;
+            printf("[0x%04x]\tmuls r%d, r%d\n", instruction, r2+16, r1+16);
+        } else if ((instruction & 0xFF88) == 0x0300) {
+            // MULSU
+            const u16 r1 = instruction & 0x7;
+            const u16 r2 = (instruction & 0x70) >> 4;
+            printf("[0x%04x]\tmulsu r%d, r%d\n", instruction, r2+16, r1+16);
+        } else if ((instruction & 0xFF88) == 0x0308) {
+            // FMUL
+            const u16 r1 = instruction & 0x7;
+            const u16 r2 = (instruction & 0x70) >> 4;
+            printf("[0x%04x]\tfmul r%d, r%d\n", instruction, r2+16, r1+16);
+        } else if ((instruction & 0xFF88) == 0x0380) {
+            // FMULS
+            const u16 r1 = instruction & 0x7;
+            const u16 r2 = (instruction & 0x70) >> 4;
+            printf("[0x%04x]\tfmuls r%d, r%d\n", instruction, r2+16, r1+16);
+        } else if ((instruction & 0xFF88) == 0x0388) {
+            // FMULSU
+            const u16 r1 = instruction & 0x7;
+            const u16 r2 = (instruction & 0x70) >> 4;
+            printf("[0x%04x]\tfmulsu r%d, r%d\n", instruction, r2+16, r1+16);
+        } else if ((instruction >> 14) == 0) {
+            // 2-op instruction
+            const u16 r1 = (instruction & 0xF) | ((instruction & 0x200) >> 5);
+            const u16 r2 = (instruction & 0x1F0) >> 4;
+            const u8 op = (instruction & 0x3C00) >> 10;
+            const char * const opnames[] = {"", "cpc", "sbc", "add", "cpse", "cp", "sub", "adc", "and", "eor", "or", "mov"};
+            printf("[0x%04x]\t%s r%d, r%d\n", instruction, opnames[op], r1, r2);
+        } else if ((instruction & 0xF000) == 0x3000) {
+            // CPI
+            const u16 value = ((instruction >> 4) & 0xF0) + (instruction & 0xF);
+            const u16 reg = (instruction >> 4) & 0xF;
+            printf("[0x%04x]\tcpi r%d, 0x%x\n", instruction, reg+16, value);
+        } else if ((instruction >> 14) == 0x1) {
+            // register-immediate instructions
+            const u16 value = ((instruction >> 4) & 0xF0) + (instruction & 0xF);
+            const u16 reg = (instruction >> 4) & 0xF;
+            const u16 op = (instruction & 0x3000) >> 12;
+            const char * const opnames[] = {"sbci", "subi", "ori", "andi"};
+            printf("[0x%04x]\t%s r%d, 0x%x\n", instruction, opnames[op], reg+16, value);
+        } else if ((instruction >> 10) == 0x24) {
+            // load/store instructions
+            const u16 reg = (instruction & 0x01F0) >> 4;
+            const u8 op = (instruction & 0xF) | ((instruction & 0x200) >> 5);
+            const char * const opnames[] = {
+                "lds", "ld", "ld", "", "lpm",    "lpm",    "elpm",    "elpm",
+                "",    "ld", "ld", "", "ld",     "ld",     "ld",      "pop",
+                "sts", "st", "st", "", "xch z,", "las z,", "lac z,", "lat z,",
+                "",    "st", "st", "", "st",     "st",     "st",     "push"
+            };
+            const char * const after[] = {
+                "", ", y+", ", -y", "", ", z", ", z+", ", z",  ", z+",
+                "", ", z+", ", -z", "", ", x", ", x+", ", -x", "",
+                "", ", y+", ", -y", "", "",    "",     "",     "",
+                "", ", z+", ", -z", "", ", x", ", x+", ", -x", ""
+            };
+            printf("[0x%04x]\t%s r%d%s\n", instruction, opnames[op], reg, after[op]);
+        } else if ((instruction & 0xD000) == 0x8000) {
+            // LDD/STD
+            const u16 reg = (instruction & 0x01F0) >> 4;
+            const u16 value = (instruction & 0x7) | ((instruction >> 7) & 0x0018) | ((instruction >> 7) & 0x0020);
+            const u8 y = (instruction & 0x8) >> 3;
+            const u8 s = (instruction & 0x0200) >> 9;
+            const char * const opnames[] = {"ldd", "std"};
+            const char * const after[] = {", z", ", y"};
+            printf("[0x%04x]\t%s r%d%s+%d\n", instruction, opnames[s], reg, after[y], value);
+        } else if ((instruction & 0xFE08) == 0x9400) {
+            // 1-op instructions
+            const u16 reg = (instruction & 0x01F0) >> 4;
+            const u8 op = instruction & 0x7;
+            const char * const opnames[] = {"com", "neg", "swap", "inc", "", "asr", "lsr", "ror"};
+            if (op == 4) {
+                error("opcode 0x9408 doesn't exist ");
+            }
+            printf("[0x%04x]\t%s r%d\n", instruction, opnames[op], reg);
+        } else if ((instruction & 0xFF0F) == 0x9408) {
+            // SEx/CLx
+            const u16 bit = (instruction & 0x0070) >> 4;
+            const u8 op = (instruction & 0x0080) >> 7;
+            const char * const bitnames = "cznvshti";
+            const char * const opnames[] = {"se", "cl"};
+            printf("[0x%04x]\t%s%c\n", instruction, opnames[op], bitnames[bit]);
+        } else if ((instruction & 0xFF0F) == 0x9508) {
+            // 0-op instructions
+            const u8 op = (instruction & 0xF0) >> 4;
+            const char * const opnames[] = {
+                "ret", "reti", "", "", "", "", "", "",
+                "sleep", "break", "wdr", "", "lpm", "elpm", "spm", "spm z+"};
+            if (opnames[op] == "") {
+                error("0-op opcode doesn't exist");
+            }
+            printf("[0x%04x]\t%s\n", instruction, opnames[op]);
+        } else if ((instruction & 0xFEEF) == 0x9409) {
+            // Indirect jump/call to Z or EIND:Z
+            const u8 op = ((instruction & 0x0100) >> 7) | (instruction & 0x0010) >> 4;
+            const char * const opnames[] = {"ijmp", "eijmp", "icall", "eicall"};
+            printf("[0x%04x]\t%s\n", instruction, opnames[op]);
+        } else if ((instruction & 0xFE0F) == 0x940A) {
+            // DEC
+            const u16 reg = (instruction & 0x01F0) >> 4;
+            printf("[0x%04x]\tdec r%d\n", instruction, reg);
+        } else if ((instruction & 0xFF0F) == 0x940B) {
+            // DES
+            const u16 value = (instruction & 0x00F0) >> 4;
+            printf("[0x%04x]\tdes 0x%x\n", instruction, value);
+        } else if ((instruction & 0xFE00) == 0x9600) {
+            // ADIW/SBIW
+            const u8 value = (instruction & 0x000F) | ((instruction & 0x00C0) >> 2);
+            const u8 reg = (instruction & 0x0030) >> 4;
+            const u8 op = (instruction & 0x0100) >> 8;
+            const char * const opnames[] = {"adiw", "sbiw"};
+            printf("[0x%04x]\t%s r%d, 0x%x\t", instruction, opnames[op], (reg+12)*2, value);
+        } else if ((instruction & 0xFC00) == 0x9800) {
+            // CBI/SBI/SBIC/SBIS
+            const u8 value = (instruction & 0x00F8) >> 3;
+            const u8 bit = instruction & 0x7;
+            const u8 op = (instruction & 0x0300) >> 8;
+            const char * const opnames[] = {"cbi", "sbic", "sbi", "sbis"};
+            printf("[0x%04x]\t%s 0x%x, 0x%x\n", instruction, opnames[op], value, bit);
+        } else if ((instruction & 0xFC00) == 0x9C00) {
+            // MUL, unsigned: R1:R0 = Rr × Rd
+            const u16 r1 = (instruction & 0xF) | ((instruction & 0x200) >> 5);
+            const u16 r2 = (instruction & 0x1F0) >> 4;
+            printf("[0x%04x]\tmul r%d, r%d\n", instruction, r2, r1);
+        } else if ((instruction & 0xF000) == 0xB000) {
+            // IN/OUT to I/O space
+            const u16 address = (instruction & 0xF) | ((instruction & 0x600) >> 5);
+            const u16 reg = (instruction & 0x1F0) >> 4;
+            if (instruction & 0x0800) {
+                printf("[0x%04x]\tout 0x%x, r%d\n", instruction, address, reg);
+            } else {
+                printf("[0x%04x]\tin r%d, 0x%x\n", instruction, reg, address);
+            }
+        } else if ((instruction & 0xE000) == 0xC000) {
+            // RJMP/RCALL
+            s16 offset = instruction & 0x0FFF;
+            if (offset & 0x0800) {
+                offset -= 4096;
+            }
+            if (instruction & 0x1000) {
+                printf("[0x%04x]\trcall %d\n", instruction, offset);
+            } else {
+                printf("[0x%04x]\trjmp %d\n", instruction, offset);
+            }
+        } else if ((instruction >> 12) == 0xE) {
+            // LDI
+            const u16 value = ((instruction >> 4) & 0xF0) + (instruction & 0xF);
+            const u16 reg = (instruction >> 4) & 0xF;
+            printf("[0x%04x]\tldi r%d, 0x%x\n", instruction, reg+16, value);
+        } else if ((instruction & 0xF800) == 0xF000) {
+            // BRBC/BRBS
+            const s8 offset = (instruction & 0x03F8) >> 3;
+            const u8 bit = (instruction & 0x7);
+            if (instruction & 0x0400) {
+                printf("[0x%04x]\tbrbs %u, %d\n", instruction, bit, offset);
+            } else {
+                printf("[0x%04x]\tbrbc %u, %d\n", instruction, bit, offset);
+            }
+        } else if ((instruction & 0xFC08) == 0xF800) {
+            //BLD/BST
+            const u8 bit = (instruction & 0x7);
+            const u16 reg = (instruction >> 4) & 0xF;
+            if (instruction & 0x0200) {
+                printf("[0x%04x]\tbst r%d, %u\n", instruction, reg, bit);
+            } else {
+                printf("[0x%04x]\tbld r%d, %u\n", instruction, reg, bit);
+            }
+        } else if ((instruction & 0xFC08) == 0xFC00) {
+            // SBRC/SBRS
+            const u8 bit = (instruction & 0x7);
+            const u16 reg = (instruction >> 4) & 0xF;
+            if (instruction & 0x0200) {
+                printf("[0x%04x]\tsbrs r%d, %u\n", instruction, reg, bit);
+            } else {
+                printf("[0x%04x]\tsbrc r%d, %u\n", instruction, reg, bit);
+            }
+        } else {
+            error("You have invented a new AVR instruction, gz");
+        }
     }
 }
 
-void IR2AVR(const std::vector<IR> &ir) {
-    // https://en.wikipedia.org/wiki/Atmel_AVR_instruction_set#Instruction_encoding
-    // http://ww1.microchip.com/downloads/cn/DeviceDoc/AVR-Instruction-Set-Manual-DS40002198A.pdf
+void IR2AVR(const std::vector<IR> &ir, std::vector<u32> &AVRinstructions) {
     // NOTE(mdizdar): currently IR instructions are 1 to 1 with AVR ones, this shouldn't be the case since the IR should assume e.g. multiplication can be done between any two registers (of which there's an infinite number)
     // TODO(mdizdar): ... so, there should be a table of identifiers so the mapping can be done correctly
     for (size_t i = 0; i < ir.size(); ++i) {
         switch (ir[i].instruction) {
             case IRt::nop: {
-                printAVR(0);
+                AVRinstructions.push_back(0);
                 break;
             }
             case IRt::mov: {
                 if (ir[i].imm == 0) { // rr
                     const u32 r1 = ir[i].operands[0], r2 = ir[i].operands[1];
-                    printAVR((0x2C00 | (r1 & 0xF) | ((r1 & 0x10) << 4) | (r2 << 4)) << 16);
+                    AVRinstructions.push_back((0x2C00 | (r1 & 0xF) | ((r1 & 0x10) << 4) | (r2 << 4)) << 16);
                 } else if (ir[i].imm == 2) { // cr
                     const u32 value = ir[i].operands[0];
                     const u32 reg = ir[i].operands[1];
-                    printAVR((0xE000 | ((value & 0xF0) << 4) | (value & 0xF) | (reg << 4)) << 16);
+                    AVRinstructions.push_back((0xE000 | ((value & 0xF0) << 4) | (value & 0xF) | (reg << 4)) << 16);
                 } else {
                     error("imm corrupt or your mov operands don't make sense");
                 }
@@ -146,11 +294,11 @@ void IR2AVR(const std::vector<IR> &ir) {
             case IRt::add: {
                 if (ir[i].imm == 0) { // rr
                     const u32 r1 = ir[i].operands[0], r2 = ir[i].operands[1];
-                    printAVR((0x1C00 | (r1 & 0xF) | ((r1 & 0x10) << 4) | (r2 << 4)) << 16);
+                    AVRinstructions.push_back((0x1C00 | (r1 & 0xF) | ((r1 & 0x10) << 4) | (r2 << 4)) << 16);
                 } else if (ir[i].imm == 2) { // cr
                     const u32 value = ir[i].operands[0];
                     const u32 reg = ir[i].operands[1];
-                    printAVR((0x4000 | ((value & 0xF0) << 4) | (value & 0xF) | (reg << 4)) << 16);
+                    AVRinstructions.push_back((0x4000 | ((value & 0xF0) << 4) | (value & 0xF) | (reg << 4)) << 16);
                 } else {
                     error("imm corrupt or your add operands don't make sense");
                 }
@@ -216,25 +364,25 @@ void IR2AVR(const std::vector<IR> &ir) {
             }
             case IRt::ret: {
                 // if it's already in the registers, just return?
-                printAVR(0x95080000);
+                AVRinstructions.push_back(0x95080000);
                 break;
             }
             case IRt::retc: {
                 u32 value = ir[i].operands[0];
-                printAVR((0xE080 | ((value & 0xF0) << 4) | (value & 0xF)) << 16);
+                AVRinstructions.push_back((0xE080 | ((value & 0xF0) << 4) | (value & 0xF)) << 16);
                 value >>= 8;
-                printAVR((0xE090 | ((value & 0xF0) << 4) | (value & 0xF)) << 16);
-                printAVR(0x95080000);
+                AVRinstructions.push_back((0xE090 | ((value & 0xF0) << 4) | (value & 0xF)) << 16);
+                AVRinstructions.push_back(0x95080000);
                 break;
             }
             case IRt::pop: { // pop into register
                 const u32 reg = ir[i].operands[0];
-                printAVR((0x900F | (reg << 4)) << 16);
+                AVRinstructions.push_back((0x900F | (reg << 4)) << 16);
                 break;
             }
             case IRt::push: { // push from register, consider adding push from constant
                 const u32 reg = ir[i].operands[0];
-                printAVR((0x920F | (reg << 4)) << 16);
+                AVRinstructions.push_back((0x920F | (reg << 4)) << 16);
                 break;
             }
             case IRt::cmp: { // do we need this?
--- IR2AVR/driver.cpp
+++ IR2AVR/driver.cpp
@@ -1,6 +1,14 @@
 #include <vector>
 #include "IR2AVR.hpp"
 
+inline u16 swapendiannes16(u16 x) {
+    return ((x & 0xFF00) >> 8) | ((x & 0x00FF) << 8);
+}
+
+inline u32 swapendiannes(u32 x) {
+    return (swapendiannes16((x & 0xFFFF0000) >> 16) << 16) | swapendiannes16(x & 0x0000FFFF);
+}
+
 int main(int argc, char **argv) {
     std::vector<IR> ir = {
         {IRt::nop, {}, 0},
@@ -13,6 +21,64 @@ int main(int argc, char **argv) {
         {IRt::add, {3, 5}, 0},
         {IRt::add, {71, 3}, 0x2},
     };
-    IR2AVR(ir);
+    std::vector<u32> AVRinstructions = {
+        0x0C942A00,
+        0x0C943600,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x0C943400,
+        0x11240000,
+        0x1FBE0000,
+        0xCFE50000,
+        0xD4E00000,
+        0xDEBF0000,
+        0xCDBF0000,
+        0x0E944500,
+        0x0C944800,
+        0x0C940000,
+        0x1F920000,
+        0x0F920000,
+        0x0FB60000,
+        0x0F920000,
+        0x11240000,
+        0x8F930000,
+        0x8BB30000,
+        0x880F0000,
+        0x8BBB0000,
+        0x8F910000,
+        0x0F900000,
+        0x0FBE0000,
+        0x0F900000,
+        0x1F900000,
+        0x18950000,
+        0x8AE20000,
+        0x90E00000,
+        0x08950000,
+        0xF8940000,
+        0xFFCF0000,
+    };
+    for (u32 &e: AVRinstructions) {
+        //printf("%x\n", e);
+        e = swapendiannes(e);
+        //printf("%x\n", e);
+    }
+    //IR2AVR(ir, AVRinstructions);
+    printAVR(AVRinstructions);
     return 0;
 }
