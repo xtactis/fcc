0cb45cb94e3396f891501c5e8a681d36a7edc677
xtactis <xtactis@gmail.com>
2020-12-29 05:27:35 +0100

    told cl to use the correct C standard

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-set opts=-FC -GR- -EHa- -nologo -Zi
+set opts=-FC -GR- -EHa- -nologo -Zi -std:c17
 set code=%cd%
 pushd bin
 cl %opts% %code%\main.c -Fefcc.exe
