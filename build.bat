b4035a5e11659d348d4b8b8e3f5db2209eab3686
xtactis <xtactis@gmail.com>
2020-12-30 22:56:34 +0100

    added some optimization flags, I'll probably move these over to a release build script once building becomes slow (or if I learn to use a debugger

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-set opts=-FC -GR- -EHa- -nologo -Zi -std:c17
+set opts=-FC -GR- -EHa- -nologo -Zi -std:c17 -O2 -Oi
 set code=%cd%
 pushd bin
 cl %opts% %code%\main.c -Fefcc.exe
