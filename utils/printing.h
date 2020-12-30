55c8895b15c6c06526a36cee81ac6f0da889a963
xtactis <xtactis@gmail.com>
2020-12-30 04:43:48 +0100

    made the error thingy print out the line number on its own

--- utils/printing.h
+++ utils/printing.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+#include "types.h"
+
 #define RED     "\x1B[31m"
 #define GREEN   "\x1B[32m"
 #define YELLOW  "\x1B[33m"
@@ -13,13 +15,16 @@
 #define CYAN    "\x1B[36m"
 #define WHITE   "\x1B[37m"
 #define RESET   "\x1B[0m"
-#define NOT_IMPL error("Not implemented!");
+#define NOT_IMPL error(0, "Not implemented!");
 
-_Noreturn void error(const char* fmt, ...) {
+_Noreturn void error(u64 lineno, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
     
     fputs(RED, stderr);
+    if (lineno > 0) {
+        fprintf(stderr, "[Line %llu] ", lineno);
+    }
     vfprintf(stderr, fmt, args);
     fputs(RESET "\n", stderr);
     
