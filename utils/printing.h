9c26310f8223f36896a51bc8f1dc74a6f5b1c31b
xtactis <xtactis@gmail.com>
2021-05-15 21:18:18 +0200

    internal compiler errors will be useful if nothing else, for myself

--- utils/printing.h
+++ utils/printing.h
@@ -32,6 +32,13 @@ _Noreturn void error(u64 lineno, const char* fmt, ...) {
     exit(1);
 }
 
+_Noreturn void internal_error(const char * filename, u64 line_number) {
+    fputs(RED, stderr);
+    fprintf(stderr, "Internal compiler error at %s:%llu", filename, line_number);
+    fputs(RESET "\n", stderr);
+    exit(1);
+}
+
 void warning(u64 lineno, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
