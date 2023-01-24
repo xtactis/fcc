9ca783f6acb3b75e2f1a19cadeb80d4d068a9b3e
Matija Dizdar <matija.dizdar@protonmail.com>
2023-01-24 10:23:46 +0900

    slightly more useful testing than before

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-set -e
-
 check() {
     # TODO(mdizdar): actually implement testing
-    echo "$1 ..... OK"
+    res="OK"
+    build/fcc -s $1.c -o $1
+    if [ $? != 0 ]; then
+        res="FAILED"
+    fi
+    echo "$1 ..... $res"
 }
 
 if ! [ -f build/fcc ]; then
@@ -15,15 +18,9 @@ echo "==================================="
 echo "               TESTS               "
 echo "==================================="
 
-build/fcc -s tests/scope.c -o tests/scope
-check tests/scope
-build/fcc -s tests/ternary.c -o tests/ternary
-check tests/ternary
-build/fcc -s tests/if.c -o tests/if
-check tests/if
-build/fcc -s tests/ifelse.c -o tests/ifelse
-check tests/ifelse
-build/fcc -s tests/ifelseif.c -o tests/ifelseif
-check tests/ifelseif
-build/fcc -s tests/while.c -o tests/while
-check tests/while
+check 'tests/scope'
+check 'tests/ternary'
+check 'tests/if'
+check 'tests/ifelse'
+check 'tests/ifelseif'
+check 'tests/while'
